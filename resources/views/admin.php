885ac6fb380242b1f5a1e5845f8a95de47b6e010
Alex Culango <alexculango.nst@gmail.com>
2015-09-11 21:01:45 +0800

    Modify boilerplate

--- resources/views/admin.php
+++ resources/views/admin.php
@@ -6,10 +6,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Bridal Gallery">
+    <meta name="author" content="NST Pictures Cebu">
 
-    <title>SB Admin - Bootstrap Admin Template</title>
+    <title>BG</title>
 
     <!-- Application CSS -->
     <link href="/css/app.css" rel="stylesheet" type="text/css">
@@ -33,7 +33,12 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.html">SB Admin</a>
+            <a href="/" class="navbar-brand">
+                <img src="/images/logo-brandmark-light.svg" class="logo-brandmark logo-icon"
+                     alt="Bridal Gallery" width="50"/>
+                <img src="/images/logo-wordmark-light.png" class="logo-wordmark logo-icon hidden-xs hidden-sm"
+                     alt="Bridal Gallery" width="215"/>
+            </a>
         </div>
         <!-- Top Menu Items -->
         <ul class="nav navbar-right top-nav">
@@ -139,11 +144,26 @@
         <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
         <div class="collapse navbar-collapse navbar-ex1-collapse">
             <ul class="nav navbar-nav side-nav">
-                <li class="active">
+                <li v-class="active: $route.path == '/'">
                     <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                 </li>
-                <li>
-                    <a href="charts.html"><i class="fa fa-fw fa-bar-chart-o"></i> Charts</a>
+                <li v-class="active: $route.path.indexOf('story') > 0">
+                    <a href="/story/all"><i class="fa fa-fw fa-thumb-tack"></i> Stories</a>
+                    <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
+                    <ul id="demo" class="collapse">
+                        <li>
+                            <a href="#">Dropdown Item</a>
+                        </li>
+                        <li>
+                            <a href="#">Dropdown Item</a>
+                        </li>
+                    </ul>
+                </li>
+                <li v-class="active: $route.path.indexOf('category') > 0">
+                    <a href="/category/all"><i class="fa fa-fw fa-filter"></i> Categories</a>
+                </li>
+                <li v-class="active: $route.path.indexOf('story') > 0">
+                    <a href="/keyword"><i class="fa fa-fw fa-filter"></i> Keywords</a>
                 </li>
                 <li>
                     <a href="tables.html"><i class="fa fa-fw fa-table"></i> Tables</a>
@@ -187,13 +207,13 @@
             <div class="row">
                 <div class="col-lg-12">
                     <h1 class="page-header">
-                        Dashboard <small>Statistics Overview</small>
+                        Dashboard <small>Overview</small>
                     </h1>
-                    <ol class="breadcrumb">
-                        <li class="active">
-                            <i class="fa fa-dashboard"></i> Dashboard
-                        </li>
-                    </ol>
+<!--                    <ol class="breadcrumb">-->
+<!--                        <li class="active">-->
+<!--                            <i class="fa fa-dashboard"></i> Dashboard-->
+<!--                        </li>-->
+<!--                    </ol>-->
                 </div>
             </div>
             <!-- /.row -->
